6252c14a6b7c44024885102b09d6ee52f6396dd7
ivanq3w <ivanq3w@gmail.com>
2021-08-06 23:57:15 +0300

    wip supabase module and stores

--- src/modules/supabase.ts
+++ src/modules/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { UserModule } from '~/types'
+import { useAuth } from '~/use/auth'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
@@ -7,4 +8,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export const install: UserModule = ({ app, router, isClient }) => {
   // do stuff
+  const { init } = useAuth()
+  init()
 }
